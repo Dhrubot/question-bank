90fd4ce1c659af769c9d0ce9c56cde965a109e2a
dhrubot <dhrubo.dh@gmail.com>
2021-12-03 19:00:45 -0500

    added spring boot and gradle and a home controller

---
+++ src/test/java/com/revature/questionBank/QuestionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.questionBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
